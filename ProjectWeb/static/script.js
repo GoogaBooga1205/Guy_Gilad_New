d32c236e2dc7d5d0d95b6b147e9bbe50adf0e95a
GoogaBooga1205 <guygi@post.bgu.ac.i>
2023-06-15 15:33:47 +0300

    projectpartb

--- ProjectWeb/static/script.js
+++ ProjectWeb/static/script.js
@@ -1,12 +1,35 @@
 var popup = document.getElementById('popup');
 var closeBtn = document.querySelector('.close-btn');
-var address = document.getElementById('address');
+var address = document.getElementById('location');
 
-// Check if the submit button exists on the current page
+// Function to get the current location
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+// Function to display the current position
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  localStorage.setItem('latitude', latitude);
+  localStorage.setItem('longitude', longitude);
+  console.log('latitude', latitude);
+  console.log('longitude', longitude);
+}
+
+//  event listener to the button
+var locationButton = document.getElementById('location');
+if (locationButton) {
+  locationButton.addEventListener('click', function () {
+    getLocation();
+  });
+}
+
+// Check if the submit button exists on the current page (becuse the script is for 2 paegs)
 var submitBtn = document.getElementById('submitBtn');
 if (submitBtn) {
   // Add event listener to the submit button on the first page
-  submitBtn.addEventListener('click', function(event) {
+  submitBtn.addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the form from submitting and refreshing the page
 
     // Get the values from the form inputs
@@ -15,7 +38,6 @@ if (submitBtn) {
     var activityLevel = document.getElementById('activityLevel').value;
     var height = parseInt(document.getElementById('height').value);
     var weight = parseInt(document.getElementById('weight').value);
-    var address = document.getElementById('address').value;
     var goal = document.getElementById('goal').value;
 
     // Save the values in localStorage
@@ -24,19 +46,18 @@ if (submitBtn) {
     localStorage.setItem('activityLevel', activityLevel);
     localStorage.setItem('height', height);
     localStorage.setItem('weight', weight);
-    localStorage.setItem('address', address);
     localStorage.setItem('goal', goal);
 
     // Redirect to the second page
-    if (isNaN(age) || isNaN(height) || isNaN(weight)) {
+    if (isNaN(age) || isNaN(height) || isNaN(weight) || weight < 0 || height < 0 || age < 0) {
       popup.style.display = 'block';
-      closeBtn.addEventListener('click', function() {
+      closeBtn.addEventListener('click', function () {
         popup.style.display = 'none';
       });
     } else {
       window.location.href = 'index2.html';
     }
-});
+  });
 }
 
 // Retrieve the values from localStorage on the second page
@@ -45,7 +66,6 @@ var age = localStorage.getItem('age');
 var activityLevel = localStorage.getItem('activityLevel');
 var height = localStorage.getItem('height');
 var weight = localStorage.getItem('weight');
-var address = localStorage.getItem('address');
 var goal = localStorage.getItem('goal');
 
 // Print the values to the console or display them in the HTML
@@ -54,19 +74,11 @@ console.log('Age:', age);
 console.log('Activity Level:', activityLevel);
 console.log('Height:', height);
 console.log('Weight:', weight);
-console.log('Address:', address);
 console.log('Goal:', goal);
 
 // calcs
-
-//var calories = weight * 20;
-  //document.getElementById('caloriesData').textContent = calories;
-//} else {
-  //document.getElementById('caloriesData').textContent = "Missing Data , Try Refilling The Form";
-//}
-
-var bmrMen = weight * 10 + height * 6.25 - age * 5 +5;
-var bmrWomen = weight * 10 + height * 6.25 - age * 5 -161 ;
+var bmrMen = weight * 10 + height * 6.25 - age * 5 + 5;
+var bmrWomen = weight * 10 + height * 6.25 - age * 5 - 161;
 
 // calories calculations
 var tdee = 0;
@@ -75,9 +87,9 @@ if (gender === 'male') {
     tdee = bmrMen * 1.4;
   } else if (activityLevel === 'moderately active') {
     tdee = bmrMen * 1.6;
-} else if (activityLevel === 'active') {
-  tdee = bmrMen * 1.8;
-} else {
+  } else if (activityLevel === 'active') {
+    tdee = bmrMen * 1.8;
+  } else {
     document.getElementById('caloriesData').textContent = "Missing Data, Try Refilling The Form";
   }
 } else if (gender === 'female') {
@@ -85,20 +97,22 @@ if (gender === 'male') {
     tdee = bmrWomen * 1.4;
   } else if (activityLevel === 'moderately active') {
     tdee = bmrWomen * 1.6;
-} else if (activityLevel === 'active') {
-  tdee = bmrWomen * 1.8;
-} else {
+  } else if (activityLevel === 'active') {
+    tdee = bmrWomen * 1.8;
+  } else {
     document.getElementById('caloriesData').textContent = "Missing Data, Try Refilling The Form";
   }
 } else {
   document.getElementById('caloriesData').textContent = "Missing Data, Try Refilling The Form";
 }
-var goalTdee= 0;
-if(goal=='toning'){
-  goalTdee = tdee -500;
-} else if (goal=='mass'){
-  goalTdee = tdee -500;
+
+var goalTdee = 0;
+if (goal == 'toning') {
+  goalTdee = tdee - 500;
+} else if (goal == 'mass') {
+  goalTdee = tdee - 500;
 }
+
 // Display the calculated calories if valid
 if (!isNaN(goalTdee)) {
   document.getElementById('caloriesData').textContent = goalTdee.toFixed(0);
@@ -108,25 +122,26 @@ if (!isNaN(goalTdee)) {
 
 //protein calculations
 var goalProtein = 0;
-if(goal=='toning'){
-  goalProtein = weight * 2 ;
-} else if (goal=='mass'){
-  goalProtein = weight * 1.6 ;
+if (goal == 'toning') {
+  goalProtein = weight * 2;
+} else if (goal == 'mass') {
+  goalProtein = weight * 1.6;
 }
 //fat calculations
-var goalFat =(goalTdee * 0.35)/9 ;
+var goalFat = (goalTdee * 0.35) / 9;
+
 //carbs calculations
-var goalCarbsCals =goalTdee - (goalTdee * 0.35) - (goalProtein * 4) ;
-var goalCarbs = goalCarbsCals/4 ;
+var goalCarbsCals = goalTdee - (goalTdee * 0.35) - (goalProtein * 4);
+var goalCarbs = goalCarbsCals / 4;
 
 // Display the calculated atributes if valid
 if (!isNaN(goalTdee)) {
-  document.getElementById('proteinData').textContent = goalProtein.toFixed(0) + " gr" ;
-  document.getElementById('proteinFaq').textContent = "( " + (goalProtein * 4).toFixed(0) + " calories from your daily intake )"  ;
-  document.getElementById('fatData').textContent = goalFat.toFixed(0) + " gr" ;
-  document.getElementById('fatFaq').textContent = "( " + (goalFat * 9).toFixed(0) + " calories from your daily intake )"  ;
-  document.getElementById('carbsData').textContent = goalCarbs.toFixed(0) + " gr" ;
-  document.getElementById('carbsFaq').textContent = "( " + goalCarbsCals.toFixed(0) + " calories from your daily intake )"  ;
+  document.getElementById('proteinData').textContent = goalProtein.toFixed(0) + " gr";
+  document.getElementById('proteinFaq').textContent = "( " + (goalProtein * 4).toFixed(0) + " calories from your daily intake )";
+  document.getElementById('fatData').textContent = goalFat.toFixed(0) + " gr";
+  document.getElementById('fatFaq').textContent = "( " + (goalFat * 9).toFixed(0) + " calories from your daily intake )";
+  document.getElementById('carbsData').textContent = goalCarbs.toFixed(0) + " gr";
+  document.getElementById('carbsFaq').textContent = "( " + goalCarbsCals.toFixed(0) + " calories from your daily intake )";
 } else {
   popup.style.display = 'block';
   document.getElementById('proteinData').textContent = "Missing Data";
@@ -135,9 +150,9 @@ if (!isNaN(goalTdee)) {
   document.getElementById('fatFaq').textContent = "";
   document.getElementById('carbsData').textContent = "Missing Data";
   document.getElementById('carbsFaq').textContent = "";
-  }
+}
 
-  closeBtn.addEventListener('click', function() {
-    popup.style.display = 'none';
-  });
-
+//closebutton for the validation error popup
+closeBtn.addEventListener('click', function () {
+  popup.style.display = 'none';
+});
--- ProjectWeb/static/script2.js
+++ ProjectWeb/static/script2.js
@@ -1,24 +1,28 @@
- var signbutton = document.getElementById('signbutton');
- var closeBtn = document.querySelector('.close-btn');
- if (signbutton) {
-  signbutton.addEventListener('click', function(event) {
-    event.preventDefault(); 
-        // Get the values from the form inputs
+var signbutton = document.getElementById('signbutton');
+var closeBtn = document.querySelector('.close-btn');
+
+if (signbutton) {
+  signbutton.addEventListener('click', function (event) {
+    event.preventDefault();
+    // Get the values from the form inputs
     var name = document.getElementById('name').value;
     var email = parseInt(document.getElementById('email').value);
     var password = document.getElementById('password').value;
     var password2 = document.getElementById('password2').value;
-if(password==password2){
+
+    if (password == password2) {
       window.location.href = 'index.html';
       //+put data in database
     }
-    else{
+    else {
+      //button for the validation error popup
       popup.style.display = 'block';
-      closeBtn.addEventListener('click', function() {
+      closeBtn.addEventListener('click', function () {
         popup.style.display = 'none';
-
       });
-    }}
-  )};
- 
-  
+    }
+  }
+  )
+};
+
+
