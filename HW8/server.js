f38393f1961d713f9ee25f3efce9044c8abf04f0
GoogaBooga1205 <guygi@post.bgu.ac.i>
2023-06-25 14:12:21 +0300

    letsseeifworksnow

---
+++ HW8/server.js
@@ -0,0 +1,22 @@
+//import and setup
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 2023;
+app.use(express.static(path.join(__dirname, 'HW8')));
+
+//routing
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+});
+
+app.get('/formhandler', (req, res) => {
+    res.send("Thank You");
+});
+
+//set up listen
+app.listen(port, () => {
+    console.log("server is running on port", port)
+});
